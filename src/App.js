5ab701fa9f8d983dc9a54f845c4de5b86586dba6
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-13 13:18:17 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,87 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+	{ src: "/img/helmet-1.png", matched: false },
+	{ src: "/img/potion-1.png", matched: false },
+	{ "src": "/img/ring-1.png", matched: false },
+	{ src: "/img/scroll-1.png", matched: false },
+	{ src: "/img/shield-1.png", matched: false },
+	{ "src": "/img/sword-1.png", matched: false },
+];
 
 function App() {
+	const [cards, setCards] = useState([]);
+	const [turns, setTurns] = useState(0);
+	const [choiceOne, setChoiceOne] = useState(null);
+	const [choiceTwo, setChoiceTwo] = useState(null);
+	const [disabled, setDisabled] = useState(false);
+
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({...card, id: Math.random()}));
+
+		setCards(shuffledCards);
+		setChoiceOne(null);
+		setChoiceTwo(null);
+		setTurns(0);
+	}
+
+	useEffect(() => {
+		shuffleCards();
+	}, [])
+
+	const handleChoice = (card) => {
+		choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+	}
+
+	useEffect(() => {
+		if (choiceOne && choiceTwo) {
+			setDisabled(true);
+			if (choiceOne.src === choiceTwo.src) {
+				setCards(prevCards => {
+					return prevCards.map(card =>{
+						if (card.src === choiceOne.src) {
+							return { ...card, matched: true };
+						} else {
+							return card;
+						}
+					})
+				})
+			} else {
+				console.log("Different choice");
+			}
+			setTimeout(() => resetTurn(), 1000);
+		}
+	}, [choiceOne, choiceTwo])
+
+	const resetTurn = () => {
+		setChoiceOne(null);
+		setChoiceTwo(null);
+		setTurns(prevTurns => prevTurns + 1);
+		setDisabled(false);
+	}
+
+	console.log(cards);
+
 	return (
 		<div className='App'>
 			<h1>Magic Match</h1>
-			<button>New Game</button>
+			<button onClick={shuffleCards}>New Game</button>
+			<div className="card-grid">
+				{cards.map(card => (
+					<SingleCard 
+						card={card} 
+						handleChoice={handleChoice} 
+						key={card.id}
+						flipped={card === choiceOne || card === choiceTwo || card.matched}
+						disabled={disabled}
+					/>
+				))}
+			</div>
+			<p>Turns: {turns}</p>
 		</div>
 	);
 }
