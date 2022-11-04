f759a624fdd1c1870afbf6f388a4a9a9926c4c7b
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-11-04 13:34:20 +0530

    Reference public folder through public url

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { src: "/img/helmet-1.png", matched: false },
-  { src: "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { src: "/img/scroll-1.png", matched: false },
-  { src: "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false },
+  { src: process.env.PUBLIC_URL + "/img/helmet-1.png", matched: false },
+  { src: process.env.PUBLIC_URL + "/img/potion-1.png", matched: false },
+  { "src": process.env.PUBLIC_URL + "/img/ring-1.png", matched: false },
+  { src: process.env.PUBLIC_URL + "/img/scroll-1.png", matched: false },
+  { src: process.env.PUBLIC_URL + "/img/shield-1.png", matched: false },
+  { "src": process.env.PUBLIC_URL + "/img/sword-1.png", matched: false },
 ];
 
 function App() {
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -13,7 +13,7 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
         <img className="front" src={card.src} alt="Card Front" />
         <img
           className="back"
-          src="/img/cover.png"
+          src={process.env.PUBLIC_URL + "/img/cover.png"}
           alt="Card Back"
           onClick={handleClick}
         />
